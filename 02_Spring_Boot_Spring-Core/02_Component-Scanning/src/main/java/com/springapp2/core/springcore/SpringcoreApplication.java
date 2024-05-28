2f2c63d13c0f1cc66b443985bac5330bf11834ad
TITAS <karmakartitas2021@outlook.com>
2024-05-29 00:01:51 +0530

    Add files via upload

---
+++ 02_Spring_Boot_Spring-Core/02_Component-Scanning/src/main/java/com/springapp2/core/springcore/SpringcoreApplication.java
@@ -0,0 +1,19 @@
+package com.springapp2.core.springcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//@SpringBootApplication(
+//		scanBasePackages = {
+//				"com.springapp2.core.springcore","scanpackage"
+//		}
+//
+//)
+@SpringBootApplication
+public class SpringcoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcoreApplication.class, args);
+	}
+
+}
